710fff778c14c706702981b8a49a97d846aa6fd2
Edgar Torres de Freitas <edgartorresfreitas@gmail.com>
2019-05-05 12:28:30 -0300

    Código está quebrado, vou analisar depois

--- models/tasks.js
+++ models/tasks.js
@@ -1,10 +1,29 @@
-module.exports = app => {
-    return {
-        findAll: (params, callback) => {
-            return callback([
-                {title: "Fazer compras"},
-                {title: "Consertar o pc"}
-            ]);
+export default (sequelize, DataType) => {
+    const Tasks = sequelize.define("Tasks", {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataType.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        done: {
+            type: DataType.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
         }
-    };
+
+    }, {
+        classMethods: {
+            associate: (models) => {
+                Tasks.belongsTo(models.Users)
+            }
+        }
+    });
+    return Tasks;
 };
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,7 @@
 module.exports = app => {
     const Tasks = app.models.tasks;    
     app.get("/tasks", (req, res) => {
-        Tasks.findAll({}, (tasks) => {
+        Tasks.findAll({}).then(tasks => {
             res.json({tasks: tasks});
         });
     });

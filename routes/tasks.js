75de3fcd532a76f791fa3054c0e7c6e1aa3269c8
Edgar Torres de Freitas <edgartorresfreitas@gmail.com>
2019-05-08 09:10:52 -0300

    Implementado conforeme o livro  até o item 6.4

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,35 @@
 module.exports = app => {
     const Tasks = app.db.models.Tasks;
-    app.get("/tasks", (req, res) => {
-        Tasks.findAll({}).then(tasks => {
-            res.json({tasks: tasks});
+
+    app.route("/tasks")
+    .all((req, res, next) => {
+        if (req.body) delete req.body.id;
+        next();
+    })
+    .get((req, res) => {
+        Tasks.findAll({}).then(result => res.json(result))
+        .catch(error => {res.status(412).json({msg: error.message});});
+    })
+    .post((req, res) => {
+        Tasks.create(req.body)
+        .then(result => res.json(result))
+        .catch(error => {
+            res.status(412).json({msg: error.message});
         });
+    });
+
+    app.route("/tasks/:id")
+    .all((req, res, next) => {
+        if (req.body) delete req.body.id;
+        next();
+    })
+    .get((req, res) => {
+        // "/tasks/1": Consulta uma tarefa
+    })
+    .put((req, res) =>{
+        // "/tasks/1": Atualiza uma tarefa
+    })
+    .delete((req, res) => {
+        // "/tasks/1": Exclui uma tarefa uma tarefa
     });
 };

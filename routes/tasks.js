91c1950c52b0699d22648e5b66de4d38ce4ef01c
Edgar Torres de Freitas <edgartorresfreitas@gmail.com>
2019-05-09 09:00:30 -0300

    Capitulo 6 concluído, CRUD de Tasks E Users funcionando

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,10 +2,6 @@ module.exports = app => {
     const Tasks = app.db.models.Tasks;
 
     app.route("/tasks")
-    .all((req, res, next) => {
-        if (req.body) delete req.body.id;
-        next();
-    })
     .get((req, res) => {
         Tasks.findAll({}).then(result => res.json(result))
         .catch(error => {res.status(412).json({msg: error.message});});
@@ -19,17 +15,34 @@ module.exports = app => {
     });
 
     app.route("/tasks/:id")
-    .all((req, res, next) => {
-        if (req.body) delete req.body.id;
-        next();
-    })
     .get((req, res) => {
         // "/tasks/1": Consulta uma tarefa
+        Tasks.findOne({where: req.params})
+            .then(result => {
+                if (result) {
+                    res.json({result});
+                } else {
+                    res.sendStatus(404);
+                }
+            })
+            .catch(error => {
+                res.status(412).json({msg: error.message});
+            });
     })
     .put((req, res) =>{
         // "/tasks/1": Atualiza uma tarefa
+        Tasks.update(req.body, {where: req.params})
+            .then(result => res.sendStatus(204))
+            .catch(error => {
+                res.status(402).json({msg: error.message});
+            });
     })
     .delete((req, res) => {
         // "/tasks/1": Exclui uma tarefa uma tarefa
+        Tasks.destroy({where: req.params})
+            .then(result => res.sendStatus(204))
+            .catch(error => {
+                res.status(412).json({msg: error.message});
+            });
     });
 };
